b99a093332b7bb65a00d29f0bb290a04d6649840
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-10 21:02:23 +0100

    seed some tables
    adherents, contacts, adresses, depots

---
+++ seed/depots.cmd
@@ -0,0 +1,4 @@
+
+set TABLE=depots
+set PK=depot_id
+psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/%TABLE%.csv' (FORMAT CSV, header,ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
