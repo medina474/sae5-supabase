eb9b6af12cba23fdad0106d665749efd860cda10
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-22 21:43:52 +0100

    echo off sur seed

--- seed/adresses.cmd
+++ seed/adresses.cmd
@@ -1,5 +1,5 @@
+@echo off
 set PGPASSWORD=postgres
-
 set TABLE=adresses
 set PK=adresse_id
 psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/%TABLE%.csv' (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
