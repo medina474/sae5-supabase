4363f1cb18384740a1c6b3844bd70c9e62dbbc04
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-27 10:17:44 +0100

    saisons

--- seed/saisons.cmd
+++ seed/saisons.cmd
@@ -2,4 +2,4 @@
 set PGPASSWORD=postgres
 set TABLE=saisons
 set PK=saison_id
-psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/%TABLE%.csv' WITH (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
+psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/thaon/%TABLE%.csv' WITH (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
