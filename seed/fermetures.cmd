9816bba18c3865b4b68876cab65760163c4373b3
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-27 11:51:19 +0100

    Séparer les données

--- seed/fermetures.cmd
+++ seed/fermetures.cmd
@@ -2,4 +2,4 @@
 set PGPASSWORD=postgres
 set TABLE=fermetures
 set PK=fermeture_id
-psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/%TABLE%.csv' (FORMAT CSV, header,ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
+psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/thaon/%TABLE%.csv' (FORMAT CSV, header,ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
--- seed/seed.cmd
+++ seed/seed.cmd
@@ -5,19 +5,31 @@ call jardins.cmd
 call adresses.cmd
 call contacts.cmd
 
+echo saisons
 call saisons.cmd
+
+echo fermetures
 call fermetures.cmd
+
+echo feries
 call feries.cmd
 
 call preparations.cmd
 call depots.cmd
 call fournisseurs.cmd
 
+echo adherents
 call adherents.cmd
+
+echo adhesions
 call adhesions.cmd
 
 call frequences.cmd
+
+echo produits
 call produits.cmd
+
+echo paniers
 call paniers.cmd
 
 call calendriers.cmd
