ca27be019d5c862bf8f92e9da6acf9701ca35a54
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-27 21:11:58 +0100

    contacts

--- seed/02_adresses.cmd
+++ seed/02_adresses.cmd
@@ -7,5 +7,6 @@ psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../da
 psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/3-oasis/%TABLE%.csv' (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
 psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/4-lille/%TABLE%.csv' (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
 psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/5-limousin/%TABLE%.csv' (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
+psql -h localhost -p 54322 -d postgres -U postgres -c "\COPY %TABLE% FROM '../data/6-lozere/%TABLE%.csv' (FORMAT CSV, header, ENCODING 'UTF8');SELECT setval(pg_get_serial_sequence('%TABLE%', '%PK%'), (SELECT MAX(%PK%) FROM %TABLE%));"
 
 psql -h localhost -p 54322 -d postgres -U postgres -f adresses.sql
