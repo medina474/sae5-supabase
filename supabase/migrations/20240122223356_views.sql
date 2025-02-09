1f39bec9b86ea66a44c0be00ab237db83ad40546
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-02-09 19:08:50 +0100

    déplacement

--- supabase/migrations/20240122223356_views.sql
+++ supabase/migrations/20240122223356_views.sql
@@ -1,20 +1,3 @@
-create view detail_jardins
-as select j.jardin_id,
-    j.jardin,
-    j.tva,
-    a.adresse,
-    a.codepostal,
-    a.ville,
-    a.localisation,
-    c.contact,
-    c.telephone,
-    c.email
-  from jardins j
-    left join adresses a on a.adresse_id = j.adresse_id
-    left join contacts c on c.contact_id = j.contact_id;
-
-
-
 -- nombre de jours de livraison par calendrier
 create view detail_calendriers
 as select c.calendrier_id,
--- supabase/migrations/20240122223360_stat.sql
+++ supabase/migrations/20240122223360_stat.sql
@@ -4,17 +4,3 @@ select a2.saison_id, count(distinct a.adherent_id)
     join abonnements a2 on a2.adherent_id = a.adherent_id
   group by a2.saison_id;
 
-create view stat_livraisons_depots as
-select d2.depot, sum(qte) 
-from livraisons l
-join distributions d  on d.distribution_id = l.distribution_id
-join depots d2 on d2.depot_id = d.depot_id 
-group by (d2.depot);
-
-create view stat_depots_adherents as
-select d2.depot, count(distinct a.adherent_id)
-from livraisons l
-join distributions d  on d.distribution_id = l.distribution_id
-join depots d2 on d2.depot_id = d.depot_id 
-join abonnements a on a.abonnement_id = l.abonnement_id 
-group by d2.depot;
