4f7100c5ab576d93dad29616a4e72a957515a380
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-02-09 10:28:50 +0100

    remove stats

--- supabase/migrations/20240122223360_stat.sql
+++ supabase/migrations/20240122223360_stat.sql
@@ -1,34 +1,8 @@
-
-
-create view stat_adherents_profils as
-select j.jardin_id, j.jardin, p.profil, 
-    count(a.*) as nb_adherents
-  from jardins j
-    left join adherents a on a.jardin_id = j.jardin_id
-    left join profils p on p.profil_id = a.profil_id 
-  group by j.jardin_id, p.profil;
-
-create view stat_depots as
-select j.jardin_id, j.jardin,
-  count(d.*) as nb_depots
-  from jardins j
-    left join depots d on d.jardin_id = j.jardin_id
-  group by j.jardin_id;
-
-
-
-
 create view stat_adherents_abonnements as
 select a2.saison_id, count(distinct a.adherent_id) 
   from adherents a 
     join abonnements a2 on a2.adherent_id = a.adherent_id
   group by a2.saison_id;
-
-create view stat_livraisons_produits as 
-select p.produit, count(qte) 
-  from livraisons l
-join produits p on l.produit_id = p.produit_id
-group by (p.produit);
 
 create view stat_livraisons_depots as
 select d2.depot, sum(qte) 
